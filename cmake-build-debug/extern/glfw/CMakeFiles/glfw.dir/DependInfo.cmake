c1cf9f1f4f7ceaab898b91612248e63a05ef3641
Max Tiganov <mtiganov@gmail.com>
2020-04-08 18:18:17 +0300

    init

---
+++ cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/cocoa_init.m" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/cocoa_init.m.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/cocoa_joystick.m" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/cocoa_joystick.m.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/cocoa_monitor.m" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/cocoa_monitor.m.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/cocoa_time.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/cocoa_time.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/cocoa_window.m" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/cocoa_window.m.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/context.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/context.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/egl_context.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/egl_context.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/init.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/init.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/input.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/input.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/monitor.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/monitor.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/nsgl_context.m" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/nsgl_context.m.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/osmesa_context.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/osmesa_context.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/posix_thread.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/posix_thread.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/vulkan.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/vulkan.c.o"
+  "/Users/mtiganov/dev/box2d/extern/glfw/src/window.c" "/Users/mtiganov/dev/box2d/cmake-build-debug/extern/glfw/CMakeFiles/glfw.dir/src/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../extern/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/extern/sajson/CMakeFiles/sajson.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sajson.dir/sajson.cpp.o"
+  "libsajson.pdb"
+  "libsajson.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sajson.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
