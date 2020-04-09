912110abd98873d8cebb4e8497640493d0beedd8
Max Tiganov <mtiganov@gmail.com>
2020-04-09 17:17:08 +0300

    update linux build

--- linux-build.sh
+++ linux-build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Use this to build box2d on any system with a bash shell
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang
+export CC=/usr/share/mingw-w64
+export CXX=/usr/share/mingw-w64
 rm -rf build
 mkdir build
 cd build
